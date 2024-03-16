ccd577d626d7e1fd285730ace3c0f615ef8058f5
sunx35 <sunx35@yandex.ru>
2024-03-16 15:55:52 +0700

    Добавьте файлы проекта.

---
+++ ConsoleGame/main.cpp
@@ -0,0 +1,21 @@
+// Игра в консоле, где есть рамки поля, персонаж (Player). Все описано при помощи символов.
+// Персонаж может перемещаться влево-вправо.
+// Возможно персонаж будет стрелять, или отбивать мячик.
+// 
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+int main() {
+	// field
+	int field_width = 80;
+	int field_height = 80;
+	char horizontal_bound_char = '=';
+	char vertical_bound_char = '||';
+
+	vector<string> field;
+
+	// player
+}
